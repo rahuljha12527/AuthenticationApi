61c1a621ad7c90a12c788fcf8dea41c69e64a15e
rahuljha661 <rahuljha12527@gmail.com>
2020-10-17 00:22:22 +0530

    login controller

--- controllers/api/v1/userController.js
+++ controllers/api/v1/userController.js
@@ -1,5 +1,6 @@
 const User = require("../../../models/user");
 const jwt = require("jsonwebtoken");
+const { use } = require("passport");
 
 module.exports.register = async function (req, res) {
   if (!req.body.name || !req.body.email || !req.body.password) {
@@ -43,6 +44,31 @@ module.exports.register = async function (req, res) {
   }
 };
 
+module.exports.login=async function(req,res){
+    try{
+        let user=await User.findOne({email:req.body.email});
+
+        if(!user || user.password!=req.body.password){
+            return res.json(422,{
+                message:"Invalid username and password"
+            });
+        }
+        return res.json(200,{
+            message:"Sign in ,Succeddfull,here is your token and keep it safe",
+            data:{
+                token:jwt.sign(user.toJSON(),"userApi",{
+                    expiresIn:"1000000000"
+                }),
+                user,
+            },
+        });
+    }catch(err){
+        console.log("*********",err);
+        return res.json(500,{
+            message:"Internal Server Error",
+        });
+    }
+};
 
 
 
